c752834eabe1cd8c373b88d545c8471da4dee4d9
Alex Gordon <Alex.Gordon.gbs@gmail.com>
2019-07-10 19:43:49 -0400

    Initial commit: 1-application build and skeleton created 2-config for the remote REDIS database added

---
+++ src/main/java/com/alex/gordon/car/CarRepairApplication.java
@@ -0,0 +1,18 @@
+package com.alex.gordon.car;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p> Main application class.</p>
+ *
+ * @author Alex Gordon
+ */
+@SpringBootApplication
+public class CarRepairApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarRepairApplication.class, args);
+    }
+
+}
